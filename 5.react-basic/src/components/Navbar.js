04f4895adae12b8fb8b1029b6247bfb949d2750a
Alokkumar-8467 <138556514+Alokkumar-8467@users.noreply.github.com>
2024-02-05 18:35:28 +0530

    Add files via upload

---
+++ 5.react-basic/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar(){
+    return(
+        <div>
+          <nav className="bg-red-400 py-4">
+            <h1 className="text-3xl font-bold">Top Courses</h1>
+          </nav>
+        </div>
+    );
+}
+
+export default Navbar;
