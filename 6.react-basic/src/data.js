b3191516178ee93587e5acc4731829acf138db54
Alokkumar-8467 <138556514+Alokkumar-8467@users.noreply.github.com>
2024-02-11 13:14:19 +0530

    Add files via upload

---
+++ 6.react-basic/src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+    {
+        id:1,
+        name:"Paras kumar",
+        job:"Software Developer",
+        image:"https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.1448711260.1707177600&semt=ais",
+        text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solution on time and within budget. They are a true partner and an asset to any project."
+    },
+    {
+        id:2,
+        name:"Ankit Tiwari",
+        job:"Data Analist",
+        image:"https://www.wovlene.com/cm-admin/uploads/image/testimonial-2.jpg",
+        text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solution on time and within budget. They are a true partner and an asset to any project."
+    },
+    {
+        id:3,
+        name:"Ashish Chauhan",
+        job:"Backend developer",
+        image:"https://media.licdn.com/dms/image/D5603AQEqWF0b8A179Q/profile-displayphoto-shrink_400_400/0/1686168153562?e=2147483647&v=beta&t=n-UA1iWHPW6lOgvuBfG9RvZPqjMnN8FKhuIhDGT2nG0",
+        text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solution on time and within budget. They are a true partner and an asset to any project."
+    },
+    {
+        id:4,
+        name:"Prashant Gupta",
+        job:"Graphic Designer",
+        image:"https://image.mux.com/ExYWlahf4NUpGPbwuJBkELTAlSH1D01kCgaFDe00eTB2s/thumbnail.jpg?time=55.076&width=350",
+        text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solution on time and within budget. They are a true partner and an asset to any project."
+    },
+    {
+        id:5,
+        name:"Mohit Rathor",
+        job:"UI Designer",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5APAvgiZ0phY27Xcoi8Akc-_Oeiw8drEN4w&usqp=CAU",
+        text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solution on time and within budget. They are a true partner and an asset to any project."
+    }
+]
+
+export default reviews;
